ae1cd544775763d63970a57af652db5a436fcf4e
Joseph Anderson <josephtanderson91@gmail.comgit config --global user.name Joseph>
2022-02-19 14:28:11 -0600

    created scaling screen container in effort to grid out text boxes not sure of efficacy

--- code/js/cyoa.js
+++ code/js/cyoa.js
@@ -1,22 +1,98 @@
-const stylesheet = document.styleSheets[0];
-const globalStyle = stylesheet.cssRules[0].style;
+
+const gameText = document.getElementById("text-log");const stylesheet = document.styleSheets[0].cssRules;
+const windowSize = window.visualViewport;
+var screenWidth = window.visualViewport["width"];
+var screenHeight = window.visualViewport["height"];
+const globalStyle = stylesheet[0].style;
+const gameScreen = document.getElementById('display-screen');
+var gameScreenX;
+var gameScreenY;
 const settings = document.getElementById("hide-settings");
-const settingsBtn = document.getElementById("settings-btn");
-const settingsMenuBtn = document.getElementById("settings-menu-btn");
-const menuStyle = stylesheet.cssRules[5].style;
+const openSettingsBtn = document.getElementById("open-settings-btn");
+const closeSettingsBtn = document.getElementById("close-settings-menu-btn");
+const menuStyle = stylesheet[4].style;
 var menuHidden = true;
 const fsToggleBtn = document.getElementById("fullscreen-setting-toggle");
 const taToggleBtn = document.getElementById("typeanimations-setting-toggle");
 const tgToggleBtn = document.getElementById("textglow-setting-toggle");
+const monitorBorder = document.getElementById("foreground-border");
 var fullscreen = false;
 var textAnimations = true;
 var textGlow = true;
-const fsToggleBtnStyle = stylesheet.cssRules[15].style;
-const taToggleBtnStyle = stylesheet.cssRules[16].style;
-const tgToggleBtnStyle = stylesheet.cssRules[17].style;
-const screenSizeStyle = stylesheet.cssRules[3].style;
-const screenSize = stylesheet.cssRules[2].style;
+const fsToggleBtnStyle = stylesheet[14].style;
+const taToggleBtnStyle = stylesheet[15].style;
+const tgToggleBtnStyle = stylesheet[16].style;
+const screenSizeStyle = stylesheet[2].style;
+const foregroundStyle = stylesheet[30].style;
+const monitorBorderStyle = stylesheet[31].style;
+const widthArr = ["960px","640px","512px","320px"];
+const heightArr = ["720px","480px","384px","240px"];
 
+
+//screen scaling
+function fullscreenOn() {
+    fullscreen = true;
+        toggleBtn(fsToggleBtnStyle, fullscreen);
+        screenSizeStyle.width = "100vw";
+        screenSizeStyle.height = "100vh";
+        screenSizeStyle.padding = "50px";
+        foregroundStyle.visibility="hidden";
+        globalStyle.fontSize = "xx-large";
+}
+
+function fullscreenOff() {
+    screenWidth = window.visualViewport["width"];
+    screenHeight = window.visualViewport["height"];
+    if ( screenWidth<screenHeight){
+        viewMin = screenWidth;
+    } else {
+        viewMin = (screenHeight);
+    }
+    fullscreen = false;
+    //toggle visuals
+    toggleBtn(fsToggleBtnStyle, fullscreen);
+    screenScaling();
+    foregroundStyle.visibility="visible";
+    globalStyle.fontSize = "large";
+}
+
+function screenScaling() {
+    screenWidth = window.visualViewport["width"];
+    screenHeight = window.visualViewport["height"];
+    if ( screenWidth<screenHeight){
+        viewMin = screenWidth;
+    } else {
+        viewMin = screenHeight;
+    }
+    if (viewMin >= 1200) {
+        screenSizeStyle.width = widthArr[0];
+        screenSizeStyle.height = heightArr[0];
+        monitorBorderStyle.width = widthArr[0];
+        monitorBorderStyle.height = heightArr[0];
+    } else if (viewMin >=992) {
+        screenSizeStyle.width = widthArr[1];
+        screenSizeStyle.height = heightArr[1];
+        monitorBorderStyle.width = widthArr[1];
+        monitorBorderStyle.height = heightArr[1];
+    } else if (viewMin >=768) {
+        screenSizeStyle.width = widthArr[2];
+        screenSizeStyle.height = heightArr[2];
+        monitorBorderStyle.width = widthArr[2];
+        monitorBorderStyle.height = heightArr[2];
+    } else if (viewMin >= 576) {
+        screenSizeStyle.width = widthArr[3];
+        screenSizeStyle.height = heightArr[3];
+        monitorBorderStyle.width = widthArr[3];
+        monitorBorderStyle.height = heightArr[3];
+    } else if (viewMin < 576) {
+        fullscreenOn();
+    }
+}
+
+screenScaling();
+windowSize.addEventListener('resize', screenScaling);
+
+//menu visibility 
 function menuToggle () {
     if (menuHidden === true) {
     menuStyle.visibility="visible";
@@ -26,11 +102,13 @@ function menuToggle () {
     menuHidden=true;
     }
 }
-settingsBtn.addEventListener('click', menuToggle);
-settingsMenuBtn.addEventListener('click', menuToggle);
+//to open menu
+openSettingsBtn.addEventListener('click', menuToggle);
+//to close menu
+closeSettingsBtn.addEventListener('click', menuToggle);
 
+//settings menu visual toggle
 function toggleBtn (button, state) {
-    console.log(button);
     if (state) {
         button.backgroundColor="rgb(27, 161, 34)";
         button.paddingLeft="15px";
@@ -39,51 +117,57 @@ function toggleBtn (button, state) {
         button.paddingLeft="2px";
     }
 }
-
+//Fullscreen option
 function fsBtnToggle() {
     if (fullscreen) {
+        //if on turn off
         fullscreen = false;
+        //toggle visuals
         toggleBtn(fsToggleBtnStyle, fullscreen);
-        screenSizeStyle.width = "90%";
-        screenSizeStyle.height = "90%";
-        screenSizeStyle.padding = "0";
-        screenSize.width = "88vmin";
-        screenSize.height = "66vmin";
+        screenSizeStyle.padding = "8px";
+        foregroundStyle.visibility="visible";
         globalStyle.fontSize = "large";
     } else {
+        
+        //if off turn on
         fullscreen = true;
+        //toggle visuals
         toggleBtn(fsToggleBtnStyle, fullscreen);
         screenSizeStyle.width = "100vw";
         screenSizeStyle.height = "100vh";
         screenSizeStyle.padding = "50px";
-        screenSize.width = "100%";
-        screenSize.height = "100%";
+        // screenSizeStyle.border = "none";
+        foregroundStyle.visibility="hidden";
         globalStyle.fontSize = "xx-large";
     }
 }
+//text 'typing' animation option
 function taBtnToggle() {
     if (textAnimations) {
         //if on turn off
         textAnimations = false;
+        //toggle visuals
         toggleBtn(taToggleBtnStyle, textAnimations);
     } else {
         //if off turn on
         textAnimations = true;
+        //toggle visuals
         toggleBtn(taToggleBtnStyle, textAnimations);
     }
 }
+//screen 'glow' option
 function tgBtnToggle() {
     if (textGlow) {
         //if on turn off
         textGlow = false;
+        //toggle visuals
         toggleBtn(tgToggleBtnStyle, textGlow);
-        console.log(textGlow);
         globalStyle.textShadow = "none";
     } else {
         //if off turn on
         textGlow = true;
+        //toggle visuals
         toggleBtn(tgToggleBtnStyle, textGlow);
-        console.log(textGlow);
         globalStyle.textShadow = "var(--text-glow)";
     }
 }
@@ -95,91 +179,34 @@ tgToggleBtn.addEventListener('click', tgBtnToggle);
 //----------------------------------------------------------//
 
 
-const gameText = document.getElementById("text-log");
 const form = document.getElementById('form');
 const input = document.getElementById("player-input");
 
-const dialog1Arr = ['You wake up in a strange place...', 
+const dialogArr = ['You wake up in a strange place...', 
 'You see a light in the distance, and a door to you left.',
 'Do you head for the light, or try the door?'];
-let gamePrint = "";
-let newLine = "";
-let x = 0;
+var gamePrint = "";
+var newLine = "";
+var x = 0;
 
 const inventoryArr = [];
 
 function print(newLine){
         gamePrint = gamePrint /*+ "\r\n" */+ newLine + "\r\n";
-    gameText.textContent = gamePrint;
-    input.value= "";
-}
-
-for (let y=0; y<dialog1Arr.length; y++) {
-    print(dialog1Arr[y])
-    } 
-    newLine = input.value;
-    gamePrint = gamePrint + "\r\n" + newLine + "\r\n";
-    gameText.textContent = gamePrint;
-    input.value= "";
-    if (newLine === "door") {
-        x = 1;
-        newLine = /*prompt*/ "example";
-        gamePrint = gamePrint + "\r\n" + newLine + "\r\n";
         gameText.textContent = gamePrint;
-    } 
+        input.value= "";
+}
 
 function playerSubmission(event) {
-    if (x === 0) {
+    console.log(event);
         playerInput = input.value;
         print(playerInput);
-    if (playerInput === "door") {
-        if (inventoryArr.includes("key")) {
-            newLine = "You find the door, quickly force the key into the lock and force it open with all your might";
-            print(newLine);
-            x = 5;
-        } else {
-            newLine = "You lunge for the door, only to find it is locked, if only you had a key...";
-            print(newLine)
-            newLine = "Do you head for the light, or try the door, again?"
-            print(newLine)
-            x = 0;
-        }
-    } else if (playerInput === "light") {
-        newLine = "You head toward the light, uneasy of where you are...\r\n";
-        print(newLine);
-        newLine = "As you approach, "
-        x = 1; 
-    } else {
-        newLine = "I don't  understand your input, plase try again";
-        print(newLine);
-        print(dialog1Arr[1]);
-        print(dialog1Arr[2]);
-        x = 0;
-    }
-} else if (x === 1) {
-    playerInput = input.value;
-    print(playerInput);
-    
-} else if (x === 2) {
-
-} else if (x === 3) {
-    
-} else if (x === 4) {
-    
-} else if (x === 5) {
-    
-} else if (x === 6) {
-    
-}
 
 
 
-    
 //--------------------------------------
     event.preventDefault();
 }
 
 
 form.addEventListener('submit', playerSubmission);
-
-
